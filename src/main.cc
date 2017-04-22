a4ac04e9fc5bff9c3a8fe5d95311f7b68f36700c
alvarhc2 <alvarhc2@gmail.com>
2017-04-22 18:14:45 +0200

    Editando k-funciones

--- src/main.cc
+++ src/main.cc
@@ -11,7 +11,27 @@ void leer_entrada(set<string> &S){
 }
 
 void K_funciones(set<string> S) {
-	
+	int prob = 0;
+	int op;
+	while (prob <= 0 or prob >= 1000) {
+		cout <<"Escriba la maxima probabilidad de falsos positivos admitida, en tanto por mil:"<<endl;
+		cout <<"Ejemplo: 10"<<endl;
+		cout <<"Esto representa una probabilidad de falso positivo de 10 por mil,"<<endl;
+		cout <<"o lo que es lo mismo 1%."<<endl;
+		cin >> prob;
+		if(prob <= 0) {
+			cout <<"Valor erroneo, introduzca una probabilidad más elevada"<<endl;
+		}
+		else if (prob >= 1000) {
+			cout <<"Valor erroneo, introduzca una probabilidad más baja"<<endl;
+		}
+	}
+	cout <<"Elija el modo de generación de las k-funciones de hash:"<<endl;
+	cout <<"1 - Polinómica con coeficientes aleatorios (Explicada en clase)"<<endl;
+	cout <<"2 - Tabulation Hashing"<<endl;
+	cin >> op;
+	if(op == 1) {
+	}
 }
 
 void SHA(set<string> S) {
